03b4c2ddce689b755459f7a7975a67ff0fb345f1
CodePencil <jiawei.wang2021@gmail.com>
2026-08-03 23:07:23 +0800

    chore: 调整了项目打包的根路径

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-resume/' : '/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
